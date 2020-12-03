d06042ebe9f4dcd46b3e23230cdd8e4fb734c2ae
Suhel Bansal <suhelbansal@example.com>
2020-12-03 22:41:58 +0530

    Deploy To Heroku

---
+++ backend/data/products.js
@@ -0,0 +1,88 @@
+const products = [
+  {
+    name: 'GoPro Hero9 Black Camera',
+    image: '/images/GoPro.jpg',
+    description:
+      'GoPro Hero9 Black Camera with new front display, 30% more battery life, detachable lens, waterproof.',
+    brand: 'GoPro',
+    category: 'Electronics',
+    price: 50000,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'iPhone 12 Pro 256GB Memory',
+    image: '/images/iphone12Pro.jpg',
+    description:
+      'Introducing the iPhone 12 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 90000,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'JBL Pulse3 Speaker',
+    image: '/images/JBL.jpg',
+    description:
+      'JBL Pulse 3 waterproof portable Bluetooth speaker with customizable 360° lightshow and sound.',
+    brand: 'JBL',
+    category: 'Electronics',
+    price: 10000,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Apple AirPodes Pro',
+    image: '/images/AppleAirPodsPro.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 10000,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Apple Gaming Mouse',
+    image: '/images/Mouse.jpg',
+    description:
+      'Get a better handle on your games with this Apple gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 3000,
+    countInStock: 7,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Amazon Echo Dot 3rd Generation',
+    image: '/images/Alexa.jpg',
+    description:
+      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+    brand: 'Amazon',
+    category: 'Electronics',
+    price: 4000,
+    countInStock: 2,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Apple MacBook Pro',
+    image: '/images/MacBook.jpg',
+    description:
+      'Apple M1 chip with 8‑core CPU, 8‑core GPU and 16‑core Neural Engine 8GB unified memory 256GB SSD storage 13-inch Retina display with True Tone',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 90000,
+    countInStock: 2,
+    rating: 0,
+    numReviews: 0,
+  },
+]
+
+export default products
